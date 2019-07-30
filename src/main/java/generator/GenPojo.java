a32fa0e15f8abe2807156425883702e9b84fd2f6
jianying.li <lijianying12@gmail.com>
2019-07-30 23:07:16 +0800

    update

--- src/main/java/generator/GenPojo.java
+++ src/main/java/generator/GenPojo.java
@@ -1,4 +1,8 @@
 package generator;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.concurrent.atomic.AtomicLong;
 
 import com.sun.codemodel.CodeWriter;
@@ -14,38 +18,111 @@ import org.springframework.web.bind.annotation.RestController;
 public class GenPojo {
     @RequestMapping("/generator/preview")
     public String greeting(
-            @RequestParam(value="schema", defaultValue="World") String schema,
-            @RequestParam(value="targetpackage", defaultValue="com.example") String targetPackage,
-            @RequestParam(value="classname", defaultValue="Example") String className,
-            @RequestParam(value="targetlanguage", defaultValue="java") String targetLanguage,
-            @RequestParam(value="sourcetype", defaultValue="yamlschema") String yamlSchema,
-            @RequestParam(value="annotationstyle", defaultValue="jackson2") String annotationStyle,
-            @RequestParam(value="usedoublenumbers", defaultValue="true") String useDoubleNumbers,
-            @RequestParam(value="includeaccessors", defaultValue="true") String includeAccessors,
-            @RequestParam(value="includeadditionalproperties", defaultValue="true") String includeAdditionalProperties,
-            @RequestParam(value="propertyworddelimiters", defaultValue="-_") String propertWordDelimiters
+            @RequestParam(value = "schema", defaultValue = "World") String schema,
+            @RequestParam(value = "targetpackage", defaultValue = "com.example") String targetPackage,
+            @RequestParam(value = "classname", defaultValue = "Example") String className,
+            @RequestParam(value = "targetlanguage", defaultValue = "java") String targetLanguage,
+            @RequestParam(value = "sourcetype", defaultValue = "yamlschema") String sourceType,
+            @RequestParam(value = "annotationstyle", defaultValue = "jackson2") String annotationStyle,
+            @RequestParam(value = "usedoublenumbers", defaultValue = "true") String useDoubleNumbers,
+            @RequestParam(value = "includeaccessors", defaultValue = "true") String includeAccessors,
+            @RequestParam(value = "includeadditionalproperties", defaultValue = "true") String includeAdditionalProperties,
+            @RequestParam(value = "propertyworddelimiters", defaultValue = "-_") String propertWordDelimiters
     ) {
-        System.out.printf(schema);
         JCodeModel codeModel = new JCodeModel();
         GenerationConfig config = new DefaultGenerationConfig() {
             @Override
             public boolean isGenerateBuilders() { // set config option by overriding method
                 return true;
             }
-        };
 
+            @Override
+            public Language getTargetLanguage() {
+                if (targetLanguage == "scala") {
+                    return Language.JAVA;
+                } else {
+                    return Language.SCALA;
+                }
+            }
+
+            @Override
+            public SourceType getSourceType() {
+                switch (sourceType) {
+                    case "yamlschema":
+                        return SourceType.YAMLSCHEMA;
+                    case "yaml":
+                        return SourceType.YAML;
+                    case "jsonschema":
+                        return SourceType.JSONSCHEMA;
+                    default:
+                        return SourceType.JSON;
+                }
+            }
+
+            @Override
+            public AnnotationStyle getAnnotationStyle() {
+                switch (annotationStyle) {
+                    case "jackson2":
+                        return AnnotationStyle.JACKSON2;
+                    case "jackson1":
+                        return AnnotationStyle.JACKSON1;
+                    case "jackson":
+                        return AnnotationStyle.JACKSON;
+                    case "GSON":
+                        return AnnotationStyle.GSON;
+                    case "moshi1":
+                        return AnnotationStyle.MOSHI1;
+                    default:
+                        return AnnotationStyle.NONE;
+                }
+            }
+
+            @Override
+            public boolean isUseDoubleNumbers() {
+                if (useDoubleNumbers == "true") {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+            @Override
+            public boolean isIncludeDynamicAccessors() {
+                if (includeAccessors == "true") {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+            @Override
+            public boolean isIncludeAdditionalProperties() {
+                if (includeAdditionalProperties == "true") {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+            @Override
+            public char[] getPropertyWordDelimiters(){
+                return propertWordDelimiters.toCharArray();
+            }
+        };
 
         SchemaMapper mapper = new SchemaMapper(
                 new RuleFactory(config, new Jackson2Annotator(config), new SchemaStore()), new SchemaGenerator()
         );
         try {
             mapper.generate(codeModel, className, targetPackage, schema);
-            CodeWriter codeWriter = new SingleStreamCodeWriter(System.out);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            CodeWriter codeWriter = new SingleStreamCodeWriter(stream);
             codeModel.build(codeWriter);
-        }catch (Exception e){
+            stream.close();
+            return stream.toString();
+        } catch (Exception e) {
             return e.toString();
         }
 
-        return "abc";
     }
 }
